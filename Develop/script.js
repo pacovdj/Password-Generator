980cef3fbbf884bc81ac313982d1a6ea7d59cf0d
Mona <you@example.com>
2021-11-21 22:49:07 -0500

    uppercase and lowercase

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,27 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+const LOWERCASE_CHAR_CODES = arrayFromLowToHigh(65, 90)
+const UPPERCASE_CHAR_CODES = arrayFromLowToHigh(97, 122)
+const NUMBER_CHAR_CODES = arrayFromLowToHigh(48, 57)
+const SYMBOL_CHAR_CODES  = arrayFromLowToHigh(33, 47)
+
+function generatePassword(characterAmount, includeUppercase, includeNumbers, includeSymbols) {
+  let charCodes = LOWERCASE_CHAR_CODES
+  if 
+  String.fromCharCode(65)
+}
+
+function arrayFromLowToHigh(low, high) {
+  const array = []
+  for (let i = low; i<= high; i++) {
+    array.push(i)
+  }
+  return array
+}
+
+
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -12,3 +33,5 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+//comment//
